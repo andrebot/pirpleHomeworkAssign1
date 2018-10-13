d419e052b6df415080665184733ebc41e3d9c524
Andre Botelho Rodrigues Almeida <andrebot_almeida@hotmail.com>
2018-10-13 08:49:11 -0300

    Simple server which hae a single endpoint '/hello'

---
+++ index.js
@@ -0,0 +1,30 @@
+const HTTP = require('http');
+const URL = require('url');
+const config = require('./config');
+
+const helloPayload = JSON.stringify({ msg: 'Hello World!' });
+const errorPayload = JSON.stringify({ msg: 'Page not found' });
+
+function parseUrl(url) {
+  const parsedUrl = URL.parse(url);
+  const path = parsedUrl.pathname;
+
+  return path.replace(/^\/+|\/+$/g, '');
+}
+
+function routeRequest(path, response) {
+  response.setHeader('Content-Type', 'application/json');
+
+  if (path === 'hello') {
+    response.writeHead(200);
+    response.end(helloPayload);
+  } else {
+    response.writeHead(404);
+    response.end(errorPayload);
+  }
+}
+
+const httpServer = HTTP.createServer((request, response) => routeRequest(parseUrl(request.url), response));
+httpServer.listen(config.PORT, function() {
+  console.log(`Listening on port ${config.PORT}`);
+});
